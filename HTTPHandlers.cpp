02d5af8a725e9465a6df8e19edbac3eb52703536
JojoS62 <sn2010@onlinehome.de>
2020-01-03 19:26:10 +0100

    add http handlers

---
+++ HTTPHandlers.cpp
@@ -0,0 +1,224 @@
+#include "HTTPhandlers.h"
+#include "globalVars.h"
+
+Mutex mutexReqHandlerRoot;
+
+// Requests come in here
+void request_handler(HttpParsedRequest* request, ClientConnection* clientConnection) {
+    mutexReqHandlerRoot.lock();
+    HttpResponseBuilder builder(200, clientConnection);
+
+    if (request->get_method() == HTTP_GET && request->get_url() == "/format") {
+        builder.headers["Content-Type"] = "text/html; charset=utf-8";
+        builder.sendHeader();
+
+        string body = 
+            "<html><head><title>Hello from mbed</title></head>"
+            "<body>"
+                "<h1>mbed webserver</h1>"
+                "<button id=\"toggle\">Format Flash with LittleFS</button>"
+                "<script>document.querySelector('#toggle').onclick = function() {"
+                    "var x = new XMLHttpRequest(); x.open('POST', '/toggle'); x.send();"
+                "}</script>"
+            "</body></html>";
+
+        builder.sendBodyString(body);
+//    } else if(request->get_method() == HTTP_GET && request->get_url() == "/") {
+//        builder.sendHeaderAndFile(&fs, "/index.html");
+    } else if(request->get_method() == HTTP_GET && request->get_url() == "/test.svg") {
+        builder.headers["Content-Type"] = "image/svg+xml";
+        string out;
+        out.reserve(4096);
+        char temp[100];
+
+        out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
+        out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
+        out += "<g stroke=\"black\">\n";
+        int y = rand() % 130;
+        for (int x = 10; x < 390; x+= 10) {
+            int y2 = rand() % 130;
+            sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+            out += temp;
+            y = y2;
+        }
+        out += "</g>\n</svg>\n";
+        builder.sendHeader(200);
+        builder.sendBodyString(out);
+//    } else if(request->get_method() == HTTP_GET) {
+//        builder.sendHeaderAndFile(&fs, request->get_url());
+    } else if (request->get_method() == HTTP_POST && request->get_url() == "/toggle") {
+        debug("toggle LED called\n\n");
+        //led = !led;
+        builder.sendHeader();
+    } else {
+        builder.sendHeader(404);
+    }
+    mutexReqHandlerRoot.unlock();
+}
+
+Mutex mutexReqHandlerStatus;
+const char* Compilername[] = {
+    "ARM",
+    "GCC_ARM",
+    "IAR"
+};
+
+string getCPUIDame(uint32_t cpu_id) 
+{
+    uint16_t rev = cpu_id & 0xf;
+    cpu_id = (cpu_id >> 4) & 0xfff;
+
+    string id;
+    id.reserve(32);
+
+    switch (cpu_id) {
+        case 0xC20:  
+            id = "Cortex-M0";
+            break;
+        case 0xC60: 
+            id = "Cortex-M0+";
+            break;
+        case 0xC23:
+            id = "Cortex-M3";
+            break;
+        case 0xC24:
+            id = "Cortex-M4";
+            break;
+        case 0xC27:
+            id = "Cortex-M7";
+            break;
+        case 0xD20:
+            id = "Cortex-M23";
+            break;
+        case 0xD21:
+            id = "Cortex-M33";
+            break;
+        default:
+            id = "unknown";
+    }
+    id += " Rev: " + to_string(rev);
+
+    return id;    
+}
+
+const char* StateNames[] {
+        "Inactive",           /**< NOT USED */
+        "Ready",              /**< Ready to run */
+        "Running",            /**< Running */
+        "WaitingDelay",       /**< Waiting for a delay to occur */
+        "WaitingJoin",        /**< Waiting for thread to join. Only happens when using RTX directly. */
+        "WaitingThreadFlag",  /**< Waiting for a thread flag to be set */
+        "WaitingEventFlag",   /**< Waiting for a event flag to be set */
+        "WaitingMutex",       /**< Waiting for a mutex event to occur */
+        "WaitingSemaphore",   /**< Waiting for a semaphore event to occur */
+        "WaitingMemoryPool",  /**< Waiting for a memory pool */
+        "WaitingMessageGet",  /**< Waiting for message to arrive */
+        "WaitingMessagePut",  /**< Waiting for message to be send */
+        "WaitingInterval",    /**< NOT USED */
+        "WaitingOr",          /**< NOT USED */
+        "WaitingAnd",         /**< NOT USED */
+        "WaitingMailbox",     /**< NOT USED (Mail is implemented as MemoryPool and Queue) */
+
+        /* Not in sync with RTX below here */
+        "Deleted",            /**< The task has been deleted or not started */
+};
+
+void request_handler_getStatus(HttpParsedRequest* request, ClientConnection* clientConnection) 
+{
+    mutexReqHandlerStatus.lock();
+    HttpResponseBuilder builder(200, clientConnection);
+    builder.headers["Content-Type"] = "application/json; charset=utf-8";
+    builder.sendHeader();
+
+    string body;
+    body.reserve(1024);
+
+    if (request->get_method() == HTTP_GET) {
+        if (request->get_filename() == "mem") {
+            mbed_stats_heap_t heap_info;
+            mbed_stats_heap_get( &heap_info );
+
+            body += "{\"current_size\": ";
+            body += to_string(heap_info.current_size);
+            body += ", \"max_size\": ";
+            body += to_string(heap_info.max_size);
+            body += ", \"alloc_cnt\": ";
+            body += to_string(heap_info.alloc_cnt);
+            body += ", \"reserved_size\": ";
+            body += to_string(heap_info.reserved_size);
+            body += "}";
+
+            builder.sendBodyString(body);
+        } else
+        if (request->get_filename() == "cpu") {
+            mbed_stats_cpu_t stats;
+            mbed_stats_cpu_get(&stats);
+
+            body += "{\"uptime\": ";
+            body += to_string(stats.uptime / 1000000);
+            body += ", \"idle_time\": ";
+            body += to_string(stats.idle_time / 1000000);
+            body += ", \"sleep_time\": ";
+            body += to_string(stats.sleep_time / 1000000);
+            body += ", \"deep_sleep_time\": ";
+            body += to_string(stats.deep_sleep_time / 1000000);
+            body += "}";
+
+            builder.sendBodyString(body);
+        } else
+        if (request->get_filename() == "sysinfo") {
+            mbed_stats_sys_t stats;
+            mbed_stats_sys_get(&stats);
+
+            body += "{\"MBed OS Version\": ";
+            body += "\"" + to_string(MBED_MAJOR_VERSION) + "." + to_string(MBED_MINOR_VERSION) + "." + to_string(MBED_PATCH_VERSION) + "\"";
+            body += ", \"CPU Id\": ";
+            body += "\"" + getCPUIDame(stats.cpu_id) + "\"";
+            if ((stats.compiler_id > 1) && (stats.compiler_id < 3))
+            body += ", \"Compiler Id\": \"";
+            body += Compilername[stats.compiler_id - 1];
+            body += "\", \"Compiler Version\": ";
+            body += to_string(stats.compiler_version);
+            body += "}";
+
+            builder.sendBodyString(body);
+        } else
+        if (request->get_filename() == "threads") {
+            const uint MAX_THREAD_STATS = 32;
+            mbed_stats_thread_t *stats = new mbed_stats_thread_t[MAX_THREAD_STATS];
+            int count = mbed_stats_thread_get_each(stats, MAX_THREAD_STATS);
+    
+            body += "[[\"ID\",\"Name\",\"State\",\"Priority\",\"Stack Size\",\"Stack Space\"],";
+
+            for(int i = 0; i < count; i++) {
+                body += "[";
+                body += to_string(stats[i].id) + ",";
+                body += "\"" + string(stats[i].name) + "\",";
+                body += "\"" + string(StateNames[stats[i].state]) + "\",";
+                body += to_string(stats[i].priority) + ",";
+                body += to_string(stats[i].stack_size) + ",";
+                body += to_string(stats[i].stack_space);
+                if (i < count-1)
+                    body += "],";
+                else
+                    body += "]]";
+            }
+            
+            delete stats;
+
+            builder.sendBodyString(body);
+        } else
+        if (request->get_filename() == "test") {
+
+            body += "{\"test\": 42}";
+
+            builder.sendBodyString(body);
+        } else {
+            builder.sendHeader(404);
+        }
+    }
+    else {
+        builder.sendHeader(404);
+    }
+    mutexReqHandlerStatus.unlock();
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "W5500Interface.h"
 #include "HTTPServer.h"
 #include "WebsocketHandlers.h"
+#include "HTTPHandlers.h"
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
@@ -15,16 +16,13 @@ const int ON  = 1;
 
 DigitalOut      led1(PC_13);
 
-//W5500Interface   eth(&spi, PB_12, NC);       // MOSI, MISO, SCK, CS, RESET
-
 // The MAC number must be unique within the connected network. Modify as appropriate.
 uint8_t       MY_MAC[6] = { 0xaa, 0x01, 0x10, 0xaa, 0x10, 0x10 };
 
 const char          MY_IP[] = "192.168.1.181";
-const char          MY_NETMASK[] = "255.255.255.0";
-const char          MY_GATEWAY[] = "192.168.1.1";
-int                 MY_PORT = 80;
-
+//const char          MY_NETMASK[] = "255.255.255.0";
+//const char          MY_GATEWAY[] = "192.168.1.1";
+//int                 MY_PORT = 80;
 
 /**
  * @brief
@@ -35,9 +33,9 @@ int                 MY_PORT = 80;
 int main(void) {
 //    int     ret = eth.init(MY_MAC, MY_IP, MY_NETMASK, MY_GATEWAY);
 //    int     ret = eth.init(MY_IP, MY_NETMASK, MY_GATEWAY);
-    W5500Interface   eth(PB_15, PB_14, PB_13, PB_12, NC);       // MOSI, MISO, SCK, CS, RESET
+    W5500Interface   eth(PB_15, PB_14, PB_13, PB_12, PB_9);       // MOSI, MISO, SCK, CS, RESET
     
-    int     ret = eth.init(MY_MAC);   // use DHCP
+    int ret = eth.init(MY_MAC);   // use DHCP
 
     if(!ret) {
         printf("Initialized, MY_MAC: %s\n", eth.get_mac_address());
@@ -47,33 +45,34 @@ int main(void) {
         return -1;
     }
 
-    bool ret_connect = eth.connect();
-    if (!ret_connect) {
+    bool connect_err = eth.connect();
+    if (connect_err) {
+        printf("ERROR: connect %d\n", connect_err);
+    } else {
         printf("Connected, MY_IP: %s, MY_NETMASK: %s, MY_GATEWAY: %s\n",
                 eth.get_ip_address(),
                 eth.get_netmask(),
                 eth.get_gateway()
             );
-    } else {
-        printf("ERROR: connect %d", ret_connect);
+
+        HttpServer server(&eth, 2, 1);               // max 5 threads, 4 websockets
+        server.setHTTPHandler("/", &request_handler);
+        server.setHTTPHandler("/stats/", &request_handler_getStatus);
+        
+        server.setWSHandler("/ws/", WSHandler::createHandler);
+
+        nsapi_error_t res = server.start(8080);
+
+        if (res == NSAPI_ERROR_OK) {
+            //SocketAddress socketAddress;
+            //eth.get_ip_address(&socketAddress);
+            printf("Server is listening at http://%s:8080\n", eth.get_ip_address());
+        }
+        else {
+            printf("Server could not be started... %d\n", res);
+        }
     }
 
-    HttpServer server(&eth, 2, 1);               // max 5 threads, 4 websockets
-//    server.setHTTPHandler("/", &request_handler);
-//    server.setHTTPHandler("/stats/", &request_handler_getStatus);
-    
- //   server.setWSHandler("/ws/", WSHandler::createHandler);
-
-    nsapi_error_t res = server.start(8080);
-
-    if (res == NSAPI_ERROR_OK) {
-        //SocketAddress socketAddress;
-        //eth.get_ip_address(&socketAddress);
-        printf("Server is listening at http://%s:8080\n", eth.get_ip_address());
-    }
-    else {
-        printf("Server could not be started... %d\n", res);
-    }
 
     while(1) {
         led1 = !led1;
