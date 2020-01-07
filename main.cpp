00bf5ac0ea3325683f6ed930dcb3060007c658ce
JojoS62 <sn2010@onlinehome.de>
2020-01-07 17:22:44 +0100

    use globalVars

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "mbed.h"
 #include <string>
-#include "easy-connect.h"
 #include "HTTPServer.h"
 #include "WebsocketHandlers.h"
 #include "HTTPHandlers.h"
+#include "globalVars.h"
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
@@ -12,9 +12,6 @@ using namespace std;
 
 const int OFF = 0;
 const int ON  = 1;
-
-DigitalOut      led1(LED1);
-DigitalOut      testPin(PB_8);
 
 void print_mem() 
 {
@@ -37,25 +34,31 @@ int main(void) {
     printf("Mbed OS version: %d.%d.%d\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
     printf("SystemCoreClock %ld MHz\n\n", SystemCoreClock/1000000);
 
-    NetworkInterface *eth;
-    eth = easy_connect(true);
-    if(eth == nullptr) {
-        printf("ERROR: easy_connect failed\n");
+    NetworkInterface *net;
+    net = NetworkInterface::get_default_instance();
+    net->set_blocking(true);
+    if (!net) {
+        printf("Error! No network inteface found.\n");
+        return 0;
     }
+    net->connect();
 
-    HttpServer server(eth, 3, 1);               // max 5 threads, 4 websockets
+    HttpServer server(net, 3, 1);               // max 5 threads, 4 websockets
+
+    server.addStandardHeader("Server", "JojoS_Mbed_Server");
+    server.addStandardHeader("DNT", "1");
 
     server.setHTTPHandler("/", &request_handler);
     server.setHTTPHandler("/stats/", &request_handler_getStatus);
     
     server.setWSHandler("/ws/", WSHandler::createHandler);
 
-    nsapi_error_t res = server.start(8080);
+    nsapi_error_t res = server.start(80);
 
     if (res == NSAPI_ERROR_OK) {
-        //SocketAddress socketAddress;
-        //eth->get_ip_address(&socketAddress);
-        printf("Server is listening at http://%s:8080\n", eth->get_ip_address());
+        SocketAddress socketAddress;
+        net->get_ip_address(&socketAddress);
+        printf("Server is listening at http://%s:80\n", socketAddress.get_ip_address());
     }
     else {
         printf("Server could not be started... %d\n", res);
