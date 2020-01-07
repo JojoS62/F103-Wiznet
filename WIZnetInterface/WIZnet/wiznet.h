d92c71f5ed9e0d8e538660dd7ba8ab2f18cb1dca
JojoS62 <sn2010@onlinehome.de>
2020-01-07 17:17:00 +0100

    fixed include settings

--- WIZnetInterface/WIZnet/wiznet.h
+++ WIZnetInterface/WIZnet/wiznet.h
@@ -4,14 +4,14 @@
 //#include "mbed.h"
 //#include "mbed_debug.h"
 
-#define USE_W5500  // don't use this library
+#define USE_W5500
 //#define USE_W5100S // don't use this library
 //#define USE_W6100  // don't use this library
 
-//#if defined(USE_W5500)
-//#include "W5500.h"
+#if defined(USE_W5500)
+#include "W5500.h"
 //#define USE_WIZ550IO_MAC    // want to use the default MAC address stored in the WIZ550io
-//#endif
+#endif
 
 #if defined(TARGET_WIZwiki_W7500) || defined(TARGET_WIZwiki_W7500ECO)
 #include "W7500x_toe.h"
@@ -19,10 +19,6 @@
 
 #elif defined(TARGET_WIZwiki_W7500P)
 #include "W7500x_toe.h"
-
-#else
-#include "W5500.h"
-
 #endif
 
 /*
