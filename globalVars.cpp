27fa6f8415abc649ca948461985062dfa55e1748
JojoS62 <sn2010@onlinehome.de>
2020-01-07 17:21:48 +0100

    add global vars and functions add spif and lfs add format function fixed double assignment for PA

--- globalVars.cpp
+++ globalVars.cpp
@@ -2,4 +2,61 @@
 
 GlobalVars globalVars;
 
-Rotor rotor1(PC_0, PA_6, PA_7);
+SPIFBlockDevice spif(PA_7, PA_6, PA_5, PA_4, 25000000);
+LittleFileSystem fs("sd", &spif);
+
+Rotor rotor1(PB_0, PA_8, PA_9);
+
+DigitalOut      led1(LED1);
+DigitalOut      testPin(PB_8);
+
+void formatSPIFlash(FileSystem *fs)
+{
+    volatile int err;
+
+    err = spif.init();
+    debug("spif init: %d\n", err);
+    err = spif.erase(0, spif.size());
+    debug("spif erase: %d\n", err);
+//    spif.deinit();
+
+    err = fs->mount(&spif);    
+    debug("fs mount: %d\n", err);
+    if (err) {
+        err = fs->reformat(&spif);
+        debug("fs reformat: %d\n", err);
+        
+        err = fs->mount(&spif);    
+        debug("fs mount: %d\n", err);
+    }
+}
+
+void print_dir(FileSystem *fs, const char* dirname) 
+{
+    Dir dir;
+    struct dirent ent;
+
+    dir.open(fs, dirname);
+    printf("contents of dir: %s\n", dirname);
+    printf("----------------------------------------------------\n");
+
+    while (1) {
+        size_t res = dir.read(&ent);
+        if (0 == res) {
+            break;
+        }
+        printf(ent.d_name);
+        printf("\n");
+    }
+    dir.close();
+}
+
+void print_SPIF_info() 
+{
+        spif.init();
+        printf("spif size: %llu\n",         spif.size());
+        printf("spif read size: %llu\n",    spif.get_read_size());
+        printf("spif program size: %llu\n", spif.get_program_size());
+        printf("spif erase size: %llu\n",   spif.get_erase_size());
+        spif.deinit();
+}
--- globalVars.h
+++ globalVars.h
@@ -11,9 +11,24 @@ typedef struct
     float rotorPosSetpoint;
 } GlobalVars;
 
+#include "FATFileSystem.h"
+#include "SPIFBlockDevice.h"
+#include "LittleFileSystem.h"
+
+extern SPIFBlockDevice spif;
+extern LittleFileSystem fs;
+
+void formatSPIFlash(FileSystem *fs); 
+void print_SPIF_info();
+void print_dir(FileSystem *fs, const char* dirname);
 
 extern Rotor rotor1;
 
 extern GlobalVars globalVars;
+
+extern DigitalOut      led1;
+extern DigitalOut      testPin;
+
+
 
 #endif
